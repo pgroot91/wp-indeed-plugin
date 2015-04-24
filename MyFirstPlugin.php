af0713ec25f55d6d1e6a7c63462b2938b34438f4
2complex <kumar.varun1729@gmail.com>
2015-04-25 03:47:36 +0530

    Update MyFirstPlugin.php

--- MyFirstPlugin.php
+++ MyFirstPlugin.php
@@ -4,7 +4,7 @@ Plugin Name: My First Plugin
 Plugin URI: http://yourdomain.com/
 Description: A simple hello world wordpress plugin
 Version: 1.0
-Author: ashok
+Author: varun
 Author URI: http://yourdomain.com
 License: GPL
 */
